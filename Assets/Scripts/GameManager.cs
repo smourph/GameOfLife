5a437bec9a9a8c0d7f682f5fba6afcbf5506bd2b
Julien De Conti <julien.de.conti@gmail.com>
2019-02-20 22:31:24 +0100

    WIP feature(actors): initiate actors behaviour

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,8 @@ namespace GameOfLife
         [HideInInspector] public bool foxesTurn = true;
 
         private BoardManager boardManager;
+        private List<Rabbit> rabbits;
+        private List<Fox> foxes;
         private bool doingSetup = true;
 
         void Awake()
@@ -24,6 +26,8 @@ namespace GameOfLife
                 Destroy(gameObject);
             DontDestroyOnLoad(gameObject);
 
+            rabbits = new List<Rabbit>();
+            foxes = new List<Fox>();
             boardManager = GetComponent<BoardManager>();
 
             InitGameOfLife();
@@ -32,6 +36,9 @@ namespace GameOfLife
         void InitGameOfLife()
         {
             Invoke("StartGameAfterSettingUp", gameStartDelay);
+
+            rabbits.Clear();
+            foxes.Clear();
 
             boardManager.SetupScene();
 
@@ -46,11 +53,52 @@ namespace GameOfLife
         {
             if (doingSetup)
                 return;
+
+            StartCoroutine(MoveRabbits());
+            StartCoroutine(MoveFoxes());
+        }
+
+        public void AddRabbitToList(Rabbit rabbit)
+        {
+            rabbits.Add(rabbit);
+        }
+
+        public void AddFoxToList(Fox fox)
+        {
+            foxes.Add(fox);
         }
 
         public void GameOver()
         {
             enabled = false;
+        }
+
+        IEnumerator MoveRabbits()
+        {
+            yield return new WaitForSeconds(turnDelay);
+
+            if (rabbits.Count == 0)
+                yield return new WaitForSeconds(turnDelay);
+
+            for (int i = 0; i < rabbits.Count; i++)
+            {
+                rabbits[i].Move();
+                yield return new WaitForSeconds(rabbits[i].moveTime);
+            }
+        }
+
+        IEnumerator MoveFoxes()
+        {
+            yield return new WaitForSeconds(turnDelay);
+
+            if (foxes.Count == 0)
+                yield return new WaitForSeconds(turnDelay);
+
+            for (int i = 0; i < foxes.Count; i++)
+            {
+                foxes[i].Move();
+                yield return new WaitForSeconds(foxes[i].moveTime);
+            }
         }
     }
 }
