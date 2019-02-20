88df533dc5df297087a9f00e7ceac8d0b241831f
Julien De Conti <julien.de.conti@gmail.com>
2019-02-20 22:31:24 +0100

    feature(actors): create and instantiate first animals

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -7,16 +7,40 @@ namespace GameOfLife
 {
     public class BoardManager : MonoBehaviour
     {
-        public int columns = 10;
-        public int rows = 10;
+        [Serializable]
+        public class MinMaxBound
+        {
+            public int min;
+            public int max;
+
+            public MinMaxBound(int min, int max)
+            {
+                this.min = min;
+                this.max = max;
+            }
+        }
+
+        const float scaleRate = 20f;
+
+        public int columns = 20;
+        public int rows = 20;
+        public MinMaxBound rabbitsCount = new MinMaxBound(40, 60);
+        public MinMaxBound foxesCount = new MinMaxBound(7, 12);
         public GameObject[] floorModels;
+        public GameObject[] rabbitModels;
+        public GameObject[] foxModels;
 
         private Transform boardPosition;
         private List<Vector3> gridPositions = new List<Vector3>();
+        private float floorSize;
 
         public void SetupScene()
         {
             BoardSetup();
+            InitialiseList();
+
+            GenerateActorAtRandomPosition(foxModels, foxesCount.min, foxesCount.max);
+            GenerateActorAtRandomPosition(rabbitModels, rabbitsCount.min, rabbitsCount.max);
         }
 
         void BoardSetup()
@@ -24,10 +48,48 @@ namespace GameOfLife
             boardPosition = new GameObject("Board").transform;
 
             GameObject floorPrefab = floorModels[Random.Range(0, floorModels.Length)];
-            Bounds floorPrefabBounds = floorPrefab.GetComponent<MeshFilter>().sharedMesh.bounds;
+            floorSize = floorPrefab.GetComponent<MeshFilter>().sharedMesh.bounds.size.x;
 
-            GameObject floor = Instantiate(floorPrefab, new Vector3(floorPrefabBounds.size.x, 0f, 0f), Quaternion.identity) as GameObject;
+            GameObject floor = Instantiate(floorPrefab, new Vector3(floorSize, 0f, 0f), Quaternion.identity) as GameObject;
             floor.transform.SetParent(boardPosition);
+        }
+
+        void InitialiseList()
+        {
+            gridPositions.Clear();
+
+            for (int x = 1; x < columns; x++)
+            {
+                for (int z = 1; z < rows; z++)
+                {
+                    float positionX = x * floorSize / columns;
+                    float positionZ = z * floorSize / rows;
+                    gridPositions.Add(new Vector3(positionX, 0f, positionZ));
+                }
+            }
+        }
+
+        void GenerateActorAtRandomPosition(GameObject[] modelsArray, int minimum, int maximum)
+        {
+            int actorsCount = Math.Min(Random.Range(minimum, maximum + 1), gridPositions.Count);
+            for (int i = 0; i < actorsCount; i++)
+            {
+                Vector3 position = RandomPosition();
+                Quaternion angle = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f);
+                GameObject actor = Instantiate(modelsArray[Random.Range(0, modelsArray.Length)], position, angle);
+
+                // Adapt the size of the actor
+                actor.transform.localScale *= scaleRate / Math.Max(columns, rows);
+            }
+        }
+
+        Vector3 RandomPosition()
+        {
+            int randomIndex = Random.Range(0, gridPositions.Count);
+            Vector3 randomPosition = gridPositions[randomIndex];
+            gridPositions.RemoveAt(randomIndex);
+
+            return randomPosition;
         }
     }
 }
